0aa7c0b200aea17addd897dd15f517577f70633f
LAVKUSH YADAV <100487588+lavkushy145@users.noreply.github.com>
2023-01-03 20:50:58 +0530

    Add files via upload

---
+++ Old/script.js
@@ -0,0 +1,36 @@
+/*typing animation*/
+var typed = new Typed(".typing",{
+    strings:[" ","Web Designer","Web Developer","Graphic Designer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
+/*Aside */
+const nav=document.querySelector(".nav"),
+    navList = nav.querySelectorAll("li"),
+    totalNavList = navList.length,
+    allSection = document.querySelectorAll(".section"),
+    totalSection = allSection.length;
+    for(let i=0; i<totalNavList; i++){
+        const a= navList[i].querySelector("a");
+        a.addEventListener("click", function(){
+            for(let i=0; i<totalSection; i++){
+                allSection[i].classList.remove("back-section");
+            }
+            for(let j=0; j<totalNavList; j++){
+                if (navList[j].querySelector("a").classList.contains("active")){
+                    allSection[j].classList.add("back-section");
+                }
+                navList[j].querySelector("a").classList.remove("active");
+            }
+            this.classList.add("active")
+            showSection(this);
+        })
+    }
+    function showSection(element){
+        for(let i=0; i<totalSection; i++){
+            allSection[i].classList.remove("active");
+        }
+        const target=element.getAttribute("href").split("#")[1];
+        document.querySelector("#" + target).classList.add("active")
+    }
